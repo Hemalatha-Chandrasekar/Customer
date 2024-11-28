a48cbb22f5ecbbdc6468a7807cbe199d3c801f01
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-11-28 14:49:25 -0600

    Customer Commit

--- src/Customer.java
+++ src/Customer.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
 class Customer {
-    String[] customerNames = new String[5];
+    String[] customers = new String[5];
     public void inputCustomerData() {
         Scanner input = new Scanner(System.in);
 
         String fullName;
         String firstName = "";
         String lastName = "";
-        for (int i = 0; i < customerNames.length; i++) {
+        for (int i = 0; i < customers.length; i++) {
 
-            System.out.println("Enter the first name: " + (i + 1) + "");
+            System.out.println("Enter first name of the Customer: " + (i + 1) + "");
             firstName = input.nextLine();
             while (firstName.trim().isEmpty()){
-                System.out.println("First name cannot be empty" + "\n");
-                System.out.println("Enter the Frist name: " + (i + 1) + "");
+                System.out.println("First name cannot be empty, Please try again." + "\n");
+                System.out.println("Enter first name of the Customer: " + (i + 1) + "");
                 firstName = input.nextLine();
             }
-            System.out.println("Enter the last name: " + (i + 1) + "");
+            System.out.println("Enter the last name of the Customer: " + (i + 1) + "");
             lastName = input.nextLine();
             while (lastName.trim().isEmpty()) {
-                System.out.println("Last name cannot be empty" + "\n");
+                System.out.println("Last name cannot be empty,Please try again" + "\n");
                 System.out.println("Enter the last name: " + (i + 1) + "");
                 lastName = input.nextLine();
             }
-            customerNames[i] = firstName + " " + lastName;
+            customers[i] = firstName + " " + lastName;
         }
     }
 
     public void displayCustomerData () {
-        for (String name : customerNames) {
+        System.out.println("Customer List:"); // Added heading
+        for (String name : customers) {
             System.out.println(name);
         }
     }
